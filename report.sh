d27ec6bf85958f7d71fe39a4f0ecdf80642e88e9
Mariano Forti <mforti@cnea.gov.ar>
2019-02-27 18:33:20 -0300

    arreglo el formato de la fecha en metadata

--- report.sh
+++ report.sh
@@ -21,12 +21,12 @@
 set -o nounset                              # Treat unset variables as an error
 
 KEYWORD=$1
-printf -- '---\n' report_"$KEYWORD".md 
+printf -- '---\n'> report_"$KEYWORD".md 
 printf -- 'title: Resumen para la palabra clave \"%b\" \n' $KEYWORD \
-          > report_"$KEYWORD".md
-printf -- 'author: Bitacora de Mariano Daniel Forti\n' > report_"$KEYWORD".md
-printf -- 'date: Generado el %b\n' $TODAY  > report_"$KEYWORD".md
-printf -- '---\n' > report_"$KEYWORD".md
+          >> report_"$KEYWORD".md
+printf -- 'author: Bitacora de Mariano Daniel Forti\n' >> report_"$KEYWORD".md
+printf -- 'date: Generado el %b\n' $HOY >> report_"$KEYWORD".md
+printf -- '\n---\n' >> report_"$KEYWORD".md
 
 for file in */*.md
 do
