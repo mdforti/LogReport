68249f4c3980488f1251cb3ab91ada71e4134029
Mariano Forti <mforti@cnea.gov.ar>
2019-02-27 18:01:14 -0300

    arreglado un asunto con echo -e

--- report.sh
+++ report.sh
@@ -23,7 +23,7 @@ set -o nounset                              # Treat unset variables as an error
 KEYWORD=$1
 echo "" > report_"$KEYWORD".md
 
-for file in *.md */*.md
+for file in */*.md
 do
   ismatch=$(awk -v keyword=$KEYWORD  'BEGIN{RS="##"};{if(match($0, keyword)){print $0}}' $file | tee match | wc -l )
   if [ $ismatch -gt 0 ]
@@ -32,7 +32,8 @@ do
     year=${date:0:4}
     month=${date:4:2}
     day=${date:6:2}
-    echo -e "\n## $day/$month/$year\n\n###$(sed 's/\#//g' match) " >> report_"$KEYWORD".md 
+    printf "\n## $day/$month/$year\n\n###" >> report_"$KEYWORD".md 
+    sed 's/\#//g' match >> report_"$KEYWORD".md 
   fi
 done
 
