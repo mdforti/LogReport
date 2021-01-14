430b17ab87d1c50b7a1a88efa25c60ec09c01697
Mariano Forti <mariano.forti@icams.rub.de>
2021-01-14 15:29:51 +0100

    better report

--- report.sh
+++ report.sh
@@ -21,16 +21,17 @@
 set -o nounset                              # Treat unset variables as an error
 
 KEYWORD=$1
-printf -- '---\n'> report_"$KEYWORD".md 
+printf -- '---\n'> report_"${KEYWORD/ /_}".md 
 printf -- 'title: Resumen para la palabra clave \"%b\" \n' $KEYWORD \
-          >> report_"$KEYWORD".md
-printf -- 'author: Bitacora de Mariano Daniel Forti\n' >> report_"$KEYWORD".md
-printf -- 'date: Generado el %b\n' $HOY >> report_"$KEYWORD".md
-printf -- '\n---\n' >> report_"$KEYWORD".md
+          >> report_"${KEYWORD/ /_}".md
+printf -- 'author: Bitacora de Mariano Daniel Forti\n' >> report_"${KEYWORD/ /_}".md
+printf -- 'date: Generado el %b\n' $HOY >> report_"${KEYWORD/ /_}".md
+printf -- '\n---\n' >> report_"${KEYWORD/ /_}".md
 
-for file in */*.md
+# for file in */*.md
+for file in $(find -iname \*.md)
 do
-  ismatch=$(awk -v keyword=$KEYWORD -v RS='(^|\n)## ' -v IGNORECASE=1 \
+  ismatch=$(awk -v keyword="$KEYWORD"  -v RS='(^|\n)## ' -v IGNORECASE=1 \
             '{if(match($0, keyword)){print $0}}' $file \
 	    | tee match | wc -l )
   if [ $ismatch -gt 0 ]
@@ -39,8 +40,8 @@ do
     year=${date:0:4}
     month=${date:4:2}
     day=${date:6:2}
-    printf "\n## $day/$month/$year\n\n###" >> report_"$KEYWORD".md 
-    sed 's/\#//g' match >> report_"$KEYWORD".md 
+    printf "\n## $day/$month/$year\n\n###" >> report_"${KEYWORD/ /_}".md 
+    sed 's/\#//g' match >> report_"${KEYWORD/ /_}".md 
   fi
 done
 
